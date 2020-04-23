29ff1dce5de86d7c1bf0ec0a34b243466b6bea8f
DanielMcInnes <d_h_mcinnes@hotmail.com>
2020-04-23 17:17:47 +1000

    Script converts enum types to strings

--- libocppparser/src/ocpp-getbasereportrequest.adb
+++ libocppparser/src/ocpp-getbasereportrequest.adb
@@ -2,6 +2,7 @@ pragma SPARK_mode (on);
 
 with ocpp.GetBaseReportRequest;
 with Ada.Strings; use Ada.Strings;
+
 package body ocpp.GetBaseReportRequest is 
    procedure parse(msg:   in  NonSparkTypes.packet.Bounded_String;
                    msgindex: in Integer;
@@ -12,17 +13,20 @@ package body ocpp.GetBaseReportRequest is
    begin
       checkValid(msg, msgindex, packet, action, valid);
    end parse;
+
    procedure To_Bounded_String(Self: in T;
                                retval: out NonSparkTypes.packet.Bounded_String)
    is
+      strreportBase : ReportBaseEnumType.string_t.Bounded_string;
    begin
+      ReportBaseEnumType.ToString(Self.reportBase, strreportBase);
       retval := NonSparkTypes.packet.To_Bounded_String(""
                                                       & "[2," & ASCII.LF
                                                       & '"'  &  NonSparkTypes.messageid_t.To_String(Self.messageid) & '"' & "," & ASCII.LF
                                                       & '"' & NonSparkTypes.action_t.To_String(Self.action) & '"' & "," & ASCII.LF
                                                       & "{" & ASCII.LF
-                                                      & "    "requestId & ASCII.LF
-                                                      & "    "reportBase & ASCII.LF
+                                                      & "    " & '"' & "requestId" & '"' & ": " & Self.requestId'Image & ASCII.LF
+                                                      & "    " & ReportBaseEnumType.string_t.To_String(strreportBase) & ASCII.LF
                                                       & "}" & ASCII.LF
                                                       & "]", Drop => Right);
    end To_Bounded_String;
